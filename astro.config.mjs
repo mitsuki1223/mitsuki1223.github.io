ea4ed6f89d98ecf0878bf5548e0359b31d0c09ad
閷/mitsu <xingencai060@gmail.com>
2025-01-18 23:50:31 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,8 @@ import netlify from "@astrojs/netlify";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
-
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
@@ -17,14 +15,14 @@ export default defineConfig({
     sitemap(),
     robotsTxt({
       sitemap: [
-        "",
-        "",
-      ],
+        "https://mitsuki1223.github.io/sitemap-index.xml"  // Replace with your actual sitemap URL
+      ]
     }),
     solidJs(),
     UnoCSS({ injectReset: true }),
     icon(),
-    svelte()],
+    svelte()
+  ],
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
